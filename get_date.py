ce25f7fb77f9b4bca4d03ca4093ecd03844f24fd
qellex <ananim827034@mail.ru>
2025-05-28 23:17:55 +0700

    Update get_date.py

--- get_date.py
+++ get_date.py
@@ -1,15 +1,11 @@
 from sklearn import datasets
 import pandas as pd
 
-# Загрузка данных
 data = datasets.fetch_openml(name='titanic', version=1)
 
-# Преобразование данных в DataFrame
 df = pd.DataFrame(data.data, columns=data.feature_names)
 
-# Добавление целевой переменной (target) в DataFrame
 df['survived'] = data.target
 
 
-# Сохранение данных в CSV-файл
 df.to_csv('titanic.csv', index=False)
